bdd99eec117150a9c122e78d7922f91ddbf632a3
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-07-26 01:29:27 +0530

    Update facts.js

--- javascripts/facts.js
+++ javascripts/facts.js
@@ -1,3 +1,15 @@
+function randomDate(start, end) {
+    var d = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 function nasaRequest() {
     document.getElementById("alert").style.display="block";
     let xmlhttp = new XMLHttpRequest();
@@ -9,17 +21,17 @@ function nasaRequest() {
             let hdurl = data["hdurl"];
             let explanation = data["explanation"];
 
-            let date = data["date"];
-
             document.getElementById("title").innerHTML = title;
             document.getElementById("img").src = hdurl;
             document.getElementById("explaination").innerHTML = explanation;
         }
     }
 
+    let date = randomDate(new Date(2012, 0, 1), new Date());
     let queryUrl = "https://api.nasa.gov/planetary/apod?";
     let queryKey = "api_key=kIrlqTnfuq7gHKLTX6a6Wg3ZXSGkYowq62qUnu5b";
-    let queryFull = queryUrl + queryKey;
+    let queryDate = "&date="+date+"&";
+    let queryFull = queryUrl + queryKey + queryDate;
 
     xmlhttp.open("GET", queryFull, true);
     xmlhttp.send();
@@ -27,7 +39,6 @@ function nasaRequest() {
     document.getElementById("droppable-elements").style.opacity="0.5"
     document.getElementById("instruct").style.opacity="0.5"
     document.getElementById("button").style.opacity="0.5"
-    document.getElementById("nasa").style.opacity="0.5"
     document.getElementById("guide-img").style.opacity="0.5"
 }
 function closeAlert(){
@@ -36,7 +47,5 @@ function closeAlert(){
     document.getElementById("droppable-elements").style.opacity="1";
     document.getElementById("instruct").style.opacity="1";
     document.getElementById("button").style.opacity="1";
-    document.getElementById("nasa").style.opacity="1";
     document.getElementById("guide-img").style.opacity="1"
 }
-// nasaRequest().onload;
