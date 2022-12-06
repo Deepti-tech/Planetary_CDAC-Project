f46f7accc750afe2e16823997bfc31fefb30b4cc
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-12-06 16:52:58 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,39 +23,8 @@ droppableElements.forEach(elem => {
    elem.addEventListener("drop", drop);
 })
 
-function dexter_voice(){
-   function getVoices() {
-      let voices = speechSynthesis.getVoices();
-      if(!voices.length){
-        // some time the voice will not be initialized so we can call spaek with empty string
-        // this will initialize the voices 
-        let utterance = new SpeechSynthesisUtterance("");
-        speechSynthesis.speak(utterance);
-        voices = speechSynthesis.getVoices();
-      }
-      return voices;
-    }
-    
-    
-    function speak(text, voice, rate, pitch, volume) {
-      // create a SpeechSynthesisUtterance to configure the how text to be spoken 
-      let speakData = new SpeechSynthesisUtterance();
-      speakData.volume = volume; // From 0 to 1
-      speakData.rate = rate; // From 0.1 to 10
-      speakData.pitch = pitch; // From 0 to 2
-      speakData.text = text;
-      speakData.lang = 'en';
-      speakData.voice = voice;
-      
-      // pass the SpeechSynthesisUtterance to speechSynthesis.speak to start speaking 
-      speechSynthesis.speak(speakData);
-    
-    }
-   
     if ('speechSynthesis' in window) {
-   
-      let voices = getVoices();
-      let rate = 1, pitch = 0, volume = 1;
+      let rate = 1, pitch = 1.5, volume = 1;
       let text = document.getElementById("instruct").innerText;
     
       speak(text, voices[5], rate, pitch, volume);
@@ -63,7 +32,6 @@ function dexter_voice(){
     }else{
       console.log(' Speech Synthesis Not Supported 😞'); 
     }
-}
 
 function dragStart(event) {
    document.getElementById("button").style.display = 'none';
